53cf0594a67578ec47ebf3ff4b7350c6b73993dd
Rod Boev <rod.boev@gmail.com>
2021-08-19 18:21:44 -0400

    Show latlng on the page

--- src/components/Leads.js
+++ src/components/Leads.js
@@ -1,53 +1,75 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import api from 'axios';
+import { v4 as id } from 'uuid';
 import { geosearch, formatPhoneNumber } from '../lib/utils';
 
-const Leads = () => {
-  const [leads, setLeads] = useState([]);
-  const [usableLeads, setUsableLeads] = useState([]);
+const leadReducer = (state, action) => {
+  if (action.type === 'ADD_LEAD') {
+    return [ ...state, action.payload ]
+  }
+  if (action.type === 'UPDATE_LEAD') {
+    const idx = state.findIndex(item => item.id === action.id);
+    const lead = {...state[idx]};
+    lead.latlng = action.latlng;
+    const leads = [...state];
+    leads.splice(idx, 1, lead);
+    return leads;
+  }
+  if (action.type === 'DELETE_LEAD') {
+    return state.filter(lead => lead.id !== action.payload.id)
+  }
+  return state;
+}
 
-  // On load, get leads from lead-gen API
+const Leads = () => {
+  const [leads, dispatch] = useReducer(leadReducer, [])
+
   useEffect(() => {
+    // On load, get leads from lead-gen API
     (async () => {
       const response = await api.get('https://lead-gen-lite.herokuapp.com/api/all-with-contacts.json');
-      setLeads(response.data.slice(0, 5));
-    })();
-  }, []);
+      
+      response.data.slice(0, 5).forEach(lead => {
+        const address1 = lead.address;
+        const address2 = [
+          [lead.city, lead.state].filter(Boolean).join(', '),
+          lead.zip
+        ].join(' ');
+        const phone = formatPhoneNumber(lead.phone);
 
-  // When leads changes, format and geosearch the addresses
-  useEffect(() => {
-    (async () => {
-      let tmp = [];
-      leads.forEach(async lead => {
-        if (lead.address && lead.city && lead.state) {
-          const address1 = lead.address;
-          const address2 = [
-            [lead.city, lead.state].filter(Boolean).join(', '),
-            lead.zip
-          ].join(' ');
-          const phone = formatPhoneNumber(lead.phone);
-          const queryAddress = [address1, address2].join(' ');
-          
-          tmp.push({
+        dispatch({
+          type: 'ADD_LEAD',
+          payload: {
             address1,
             address2,
             phone,
-            // latlng: getLatLng([lead.address, lead.city, lead.state, lead.zip].join(' ')),
-          });
-
-          const result = await geosearch(queryAddress);
-          if (result) {
-            console.log(queryAddress + ': ' + result);
+            latlng: '',
+            id: id()
           }
-        }
+        });
       });
-      setUsableLeads(tmp);
+    })();
+  }, []);
+
+  useEffect(() => {
+    // Update latlng when lead shows up
+    (async () => {
+      const leadToUpdate = leads.find(lead => lead.latlng === '')
+
+      if (leadToUpdate) {
+        const queryAddress = [leadToUpdate.address1, leadToUpdate.address2].join(' ');
+        dispatch({
+          type: 'UPDATE_LEAD',
+          id: leadToUpdate.id,
+          latlng: await geosearch(queryAddress),
+        });
+      }
     })();
   }, [leads]);
 
   const listAddresses = () => {
-    return usableLeads.map(lead =>
-      <p>{lead.address1}<br />{lead.address2}<br />{lead.phone}<br />{lead.latlng}</p>);
+    return leads.map(lead =>
+      <p>{lead.address1}<br />{lead.address2}<br />{lead.phone}<br />[{lead.latlng && lead.latlng.join(', ')}]</p>);
   };
 
   return (
