084ec1c049c09818bc9e84ee59fb44cf9b49428a
Rod Boev <rod.boev@gmail.com>
2021-08-19 02:03:24 -0400

    Move working code into ZipcodeLayer component and clean up redundancies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { GeoJSON } from 'react-leaflet';
-import 'leaflet-defaulticon-compatibility';
-import axios from 'axios';
 import { MapContainer } from 'react-leaflet';
+import 'leaflet-defaulticon-compatibility';
 
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
@@ -10,23 +8,13 @@ import 'leaflet-geosearch/dist/geosearch.css';
 
 import './App.css';
 
-import SearchField from './components/SearchField'
-import TileLayer from './components/TileLayer'
-import TopoJSON from './components/TopoJSON'
-import ZipcodeLayer from './components/ZipcodeLayer'
+import SearchField from './components/SearchField';
+import TileLayer from './components/TileLayer';
+import ZipcodeLayer from './components/ZipcodeLayer';
 
 const apiKey = 'pk.eyJ1Ijoicm9kYm9ldiIsImEiOiJja3NncXVnMzgxbXVwMnNvbTdwMTNsdzI0In0.6n8Lu9s1o4JwyPJmMcHsNg';
 
 function App() {
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get('/data/nyc-zip-code-tabulation-areas-polygons.geojson');
-      setData(data);
-    })();
-  }, [])
-
   return (
     <div id="mapid">
       <MapContainer center={[40.705, -73.978]} zoom={10}>
